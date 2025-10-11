ca6dc6427c5030eaa51816420c75e694ca0e5a19
jaekeun0422 <jaekeun@naver.com>
2025-10-12 00:31:57 +0900

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,83 +1,110 @@
-'use client';
+import Image from "next/image";
+import Link from "next/link";
+import {Control} from "@/app/control"; // category별 조회
 
-import { useRouter } from 'next/navigation';
-import Link from 'next/link';
-
-export default function Create() {
-    const router = useRouter();
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
-            <form 
-                className="w-full max-w-2xl bg-white p-8 border border-gray-200 shadow-lg rounded-xl" 
-                onSubmit={async (e) => {
-                    e.preventDefault();
-                    const category = e.target.category.value;
-                    const subject = e.target.subject.value;
-                    const content = e.target.content.value;
-                    const options = {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({ category, subject, content, hit: 1 })
-                    };
-                    try {
-                        const resp = await fetch(process.env.NEXT_PUBLIC_API_URL + `goodword/register`, options);
-                        if (resp.ok) {
-                            console.log("Post created successfully!");
-                            e.target.reset(); // 폼의 모든 필드를 초기화합니다.
-                            alert("좋은 글이 성공적으로 등록되었습니다!");
-                        } else {
-                            console.error("Failed to create post");
-                            alert("글 등록에 실패했습니다.");
-                        }
-                    } catch (error) {
-                        console.error("An error occurred:", error);
-                        alert("오류가 발생했습니다. 다시 시도해주세요.");
-                    }
-            }}>
-                <h1 className="text-3xl font-bold text-gray-800 mb-6 text-center">좋은 글 작성</h1>
-                
-                <div className="mb-4">
-                    <input 
-                        type="text" 
-                        name="category" 
-                        placeholder="카테고리 (예: 건강, 노후, 경제, 인생, 관계)"
-                        className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300 text-gray-800" 
-                    />
-                </div>
-                
-                <div className="mb-4">
-                    <input 
-                        type="text" 
-                        name="subject" 
-                        placeholder="제목을 입력하세요" 
-                        className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300 text-gray-800" 
-                    />
-                </div>
-                
-                <div className="mb-6">
-                    <textarea 
-                        name="content" 
-                        placeholder="내용을 입력하세요" 
-                        rows="10"
-                        className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300 text-gray-800"
-                    ></textarea>
-                </div>
-                
-                <div className="text-center">
-                    <input 
-                        type="submit" 
-                        value="좋은 글 등록"
-                        className="bg-blue-500 text-white font-bold px-8 py-3 rounded-full shadow-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75 transition-all duration-300 ease-in-out cursor-pointer"
-                    />
-                </div>
-            </form>
-            <div className="mt-8">
-                <Link href="/" className="bg-gray-800 text-white font-bold px-7 py-3 rounded-full shadow-lg hover:bg-black focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-75 transition-all duration-300 ease-in-out">
-                    홈 화면으로
-                </Link>
-            </div>
+export default async function Home() {
+  let topics = [];
+  try {
+    const resp = await fetch(process.env.NEXT_PUBLIC_API_URL+`goodword/getAll`);
+    //const resp = await fetch("http://localhost:8080/goodword/getAll", { cache: "no-store" });
+    topics = await resp.json();
+  } catch (e) {
+    console.error("API fetch error:", e);
+  }
+  // morning.svg -> morning.jpg
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
+        <Image
+          src="/morning.jpg"
+          alt="goodword logo"
+          width={350}
+          height={50}
+          priority
+        />
+        <Control/>
+        <div className="hidden flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
         </div>
-    );
+        <div className="hidden row-start-3 flex gap-[24px] items-center justify-center">
+          <ol>
+            {topics.map((topic) => (
+                <li key={topic.id}>
+                  <Link href={`/read/${topic.id}`}>{topic.hit} : {topic.subject}</Link>
+                </li>
+            ))}
+          </ol>
+        </div>
+      </main>
+      <footer className="hidden row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
 }
