611b57453c21609338b3ef1404c802d61a27a830
jaekeun0422 <jaekeun@naver.com>
2025-10-14 23:01:44 +0900

    Add files via upload

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -32,7 +32,8 @@ export default function Create() {
                         body: JSON.stringify({ category, subject, content, hit: 1 })
                     };
                     try {
-                        const resp = await fetch(process.env.NEXT_PUBLIC_API_URL + `api/register`, options);
+                        //const resp = await fetch(process.env.NEXT_PUBLIC_API_URL + `api/register`, options);
+                        const resp = await fetch(`api/register`, options);
                         if (resp.ok) {
                             console.log("Post created successfully!");
                             e.target.reset(); // 폼의 모든 필드를 초기화합니다.
