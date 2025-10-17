869a4477215a54adf86d9a169b36ac1afafd4b0f
jaekeun0422 <jaekeun@naver.com>
2025-10-18 08:52:04 +0900

    Change API URL for fetching categories

--- src/app/control.js
+++ src/app/control.js
@@ -18,7 +18,7 @@ export function Control() {
             const params = new URLSearchParams({ category: category.trim() });
             url = `${process.env.NEXT_PUBLIC_API_URL}/api/findCategory1?${params.toString()}`;
         } else {
-            url = `/api/getAll`;
+            url = `yeoga/goodword`;
         }
 
         fetch(url, { method: "GET" })
